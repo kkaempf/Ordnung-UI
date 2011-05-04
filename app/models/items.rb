0365230defd8a742e504f612bb4e57f0b4e34ab3
Klaus Kämpf <kkaempf@suse.de>
2011-05-04 21:29:16 +0200

    Give Item id and to_s

--- app/models/items.rb
+++ app/models/items.rb
@@ -11,12 +11,21 @@ class Item
   field :content
   field :checksum, :type => String
   
-  attr_accessible :content
+  attr_accessible :content, :name, :dir
 
+  attr_protected :checksum
 #  validates_presence_of :name
 #  validates_presence_of :checksum
   
   before_save :compute_checksum
+
+  def id
+    self.checksum
+  end
+  
+  def to_s
+    "#{self.name}:#{self.id}"
+  end
 
   private
     #
@@ -24,7 +33,7 @@ class Item
     #  either content is set (uploaded file)
     #  or dir + name 
     def compute_checksum
-      $stderr.puts "#{self}.compute_checksum"
+#      $stderr.puts "#{self}.compute_checksum"
       if self.content
 	self.checksum = Digest::SHA1.hexdigest(content)
 	self.dir = nil
@@ -32,7 +41,7 @@ class Item
 	raise "Neither dir nor content set" unless self.dir
 	path = File.join(self.dir,self.name)
 	raise "File #{path} not readable" unless File.readable?(path)
-	$stderr.puts "Computing checksum for #{path}"
+#	$stderr.puts "Computing checksum for #{path}"
 	File.open(path) do |f|
 	  # fixme, do partial reads for large files
 	  self.checksum = Digest::SHA1.hexdigest(f.read)
