70886bf463f7c8526253ae2a81b0cdcd6a1b598d
Klaus Kämpf <kkaempf@suse.de>
2014-09-08 19:38:21 +0200

    Keep active tag on reload

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -3,9 +3,7 @@
 #
 class TagController < ApplicationController
   def activate
-    name,value = params[:name].split("=")
-    tag = Tag.find_by(:name => name, :value => value)
-    session[:active_tag] = tag._id.to_s
+    session[:active_tag] = params[:name]
     logger.info "Activate #{session[:active_tag]}"
     render :nothing => true
   end
@@ -18,5 +16,7 @@ class TagController < ApplicationController
       Tag.create(:name => name, :value => value)
       render :json => { :html => "<span class='tag'>#{params[:name]}</span>" }
     end
+  end
+  def remove
   end
 end
