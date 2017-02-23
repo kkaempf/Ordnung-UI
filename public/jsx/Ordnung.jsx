422da1765c39a51e64a389c7d605825f09e17165
Klaus Kämpf <kkaempf@suse.de>
2017-02-23 16:27:29 +0100

    Add initial .jsx files

---
+++ public/jsx/Ordnung.jsx
@@ -0,0 +1,1 @@
+ReactDOM.render(<Index name='World'/>, document.getElementById('index'))
---
+++ public/jsx/index.jsx
@@ -0,0 +1,7 @@
+var Index = React.createClass({
+
+  render: function() {
+    return(<h1>Hello {this.props.name}!</h1>)
+  }
+
+});
