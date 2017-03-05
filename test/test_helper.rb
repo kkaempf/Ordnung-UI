be3082e2d88fc3d5281541673ffef20951a05781
Klaus Kämpf <kkaempf@suse.de>
2017-03-05 18:39:38 +0100

    Define TOPLEVEL for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 ENV['RACK_ENV'] = 'test'
-
-$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), ".."))
+TOPLEVEL = File.dirname(__FILE__)
+$LOAD_PATH.unshift File.expand_path(File.join(TOPLEVEL, ".."))
 load 'bin/app'
 require 'rspec'
 require 'rack/test'
