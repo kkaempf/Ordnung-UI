e2eab4498cb28a5d8343eb8a1ee313b96883da37
Klaus Kämpf <kkaempf@suse.de>
2017-03-05 21:02:32 +0100

    Expose database client

--- lib/ordnung/database/elasticsearch.rb
+++ lib/ordnung/database/elasticsearch.rb
@@ -57,21 +57,24 @@ module Ordnung
     def initialize
       @client = Elasticsearch::Client.new # log: true
       @index = Config.elasticsearch['index']
-      puts "Elasticsearch index #{@index.inspect}"
+#      puts "Elasticsearch index #{@index.inspect}"
       provide_mappings_to_elasticsearch _mappings
+    end
+    def client
+      @client
     end
     #
     # CRUD basics
     #
     def create id, body
       obj = @client.create index: @index, type: TYPE, id: id, body: body
-      puts "Create: #{obj.inspect}"
+#      puts "Create: #{obj.inspect}"
       obj['_id']
     end
     def update id, body
       begin
         obj = @client.index index: @index, type: TYPE, id: id, body: body
-        puts "Update: #{obj.inspect}"
+#        puts "Update: #{obj.inspect}"
         obj['_id']
       rescue Elasticsearch::Transport::Transport::Errors::BadRequest => e
         Logger.error "Elasticsearch update failed for id #{id} body #{body.inspect}"
@@ -89,10 +92,10 @@ module Ordnung
         query << "#{k}:#{v}"
       end
       query = { match_all: {} } if query.empty?
-      puts "Query #{query.inspect}"
+#      puts "Query #{query.inspect}"
       begin
         obj = @client.search index: @index, q: query
-        puts "@client.search #{obj.inspect}"
+#        puts "@client.search #{obj.inspect}"
         hits = obj["hits"]
         total = hits["total"] rescue 0
         if total > 0
@@ -102,7 +105,7 @@ module Ordnung
         else
           res = []
         end
-        puts "Search: #{res.inspect}"
+#        puts "Search: #{res.inspect}"
         res
       rescue Exception => e
         Logger.error "Elasticsearch.search(#{hash.inspect}) failed: #{e}"
@@ -112,7 +115,7 @@ module Ordnung
     def read id
       begin
         obj = @client.get index: @index, type: TYPE, id: id
-        puts "Read: #{obj.inspect}"
+#        puts "Read: #{obj.inspect}"
         obj['_source']
       rescue Elasticsearch::Transport::Transport::Errors::NotFound
         nil
